255f200424c07c4e284357316c4af498b4e6cb77
NiteshKumar2003 <123361290+NiteshKumar2003@users.noreply.github.com>
2023-01-23 16:26:38 +0530

    Update NQueenSolver.java

--- NQueenSolver.java
+++ NQueenSolver.java
@@ -1,16 +1,13 @@
 public class NQueenSolver{
 	final int N = 4;
-	void printSolution(int board[][])
-	{
+	void printSolution(int board[][]) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++)
-				System.out.print(" " + board[i][j]
-								+ " ");
+				System.out.print(" " + board[i][j] + " ");
 			System.out.println();
 		}
 	}
-	boolean isSafe(int board[][], int row, int col)
-	{
+	boolean isSafe(int board[][], int row, int col) {
 		int i, j;
 		for (i = 0; i < col; i++)
 			if (board[row][i] == 1)
@@ -21,7 +18,6 @@ public class NQueenSolver{
 		for (i = row, j = col; j >= 0 && i < N; i++, j--)
 			if (board[i][j] == 1)
 				return false;
-
 		return true;
 	}
 	boolean solveNQUtil(int board[][], int col) {
@@ -32,22 +28,21 @@ public class NQueenSolver{
 				board[i][col] = 1;
 				if (solveNQUtil(board, col + 1) == true)
 					return true;
-				board[i][col] = 0; // BACKTRACK
+				board[i][col] = 0;
 			}
 		}
 		return false;
 	}
 	boolean solveNQ() {
 		int board[][] = { { 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 } };
+				{ 0, 0, 0, 0 },
+				{ 0, 0, 0, 0 },
+				{ 0, 0, 0, 0 } };
 
 		if (solveNQUtil(board, 0) == false) {
 			System.out.print("Solution does not exist");
 			return false;
 		}
-
 		printSolution(board);
 		return true;
 	}
